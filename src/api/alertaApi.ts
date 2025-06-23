40c4b9b015b9b07888ab485a556f651707ea7a26
jofer10 <jorge_galindo614@hotmail.com>
2025-06-23 17:16:59 -0500

    fix: Cambio en la ruta de la api de alertas.

--- src/api/alertaApi.ts
+++ src/api/alertaApi.ts
@@ -2,7 +2,7 @@ import api from './axiosConfig'
 import { Alerta } from '../models/aletas';
 
 export const getAlerts = async () => {
-    const response = await api.get<Alerta[]>(`/api/alerts-all`);
+    const response = await api.get<Alerta[]>(`/api/alerts/all`);
     return response.data;
 };
 export const createAlert = async (data: Alerta) => {
