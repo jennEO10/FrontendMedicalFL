5df8927bfc63e3030e370be9404a9603b17a7ce8
jofer10 <jorge_galindo614@hotmail.com>
2025-06-25 20:03:05 -0500

    fix: Se corrige el responsive del modal de notificaciones para teléfonos.

--- src/components/header/NotificationDropdown.tsx
+++ src/components/header/NotificationDropdown.tsx
@@ -121,7 +121,10 @@ export default function NotificationDropdown() {
       <Dropdown
         isOpen={isOpen}
         onClose={closeDropdown}
-        className="absolute -right-[240px] mt-[17px] flex h-[480px] w-[350px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark sm:w-[361px] lg:right-0"
+        className="absolute top-[50px] sm:top-[55px] lg:top-[52px] z-50 flex h-[480px] w-[350px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark
+        right-4 sm:right-4 lg:right-0
+        max-[750px]:left-4 max-[750px]:right-auto
+        max-[450px]:left-2 max-[450px]:w-[320px]"
       >
         <div className="flex items-center justify-between pb-3 mb-3 border-b border-gray-100 dark:border-gray-700">
           <h5 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
